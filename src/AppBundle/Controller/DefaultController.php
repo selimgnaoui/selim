dc8c2bf4942c440eab18ac4e22b8fab027c1d24d
Jihed Mzoughi <>
2019-01-28 21:47:27 +0100

    code refactoring

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -23,51 +23,10 @@ class DefaultController extends Controller
      * @Route("/show", name="show")
      */
     public function showAction(Request $request)
-    {
-        $link='https://mitarbeiterautohaus.meinauto.de/data/modelle/17d323d6fd823d9e7f8953085bab63f6';
-        $result =[];
-        $autos=file_get_contents($link);
-
-        $data =simplexml_load_string($autos);
-     // dump( $data->data->brand);
-        foreach ($data->data->brand as $brands)
-        {
-            $aux = ($brands->attributes());
-            $name=(string)($aux['name']);
-
-            foreach ($brands->modelgroup as $modelgroup)
-            {
-               foreach ($modelgroup->model as $model){
-                   $highest_price=$this->getPrice($model->discount);
-                   if (!(array_key_exists($name, $result) ) || ($result[$name] <= $highest_price))
-                       $result[$name]=$highest_price;
-               }
-
-
-
-
-            }
-
-
-        }
-        $entityManager = $this->getDoctrine()->getManager();
-
-
-        foreach ($result as $auto_name=>$price){
-          $auto = new Auto();
-          $auto->setName($auto_name);
-          $auto->setPrice($price);
-          $entityManager->persist($auto);
-
-
-
-        }
-        $entityManager->flush();
-
-     //  echo $this->getPrice('22 - 25');
-
-
-        // replace this example code with whatever you need
+    {   // get the service
+        $autoService = $this->container->get('auto_service');
+        $autos=$autoService->fetchApi();
+        $autoService->savetoDatabase($autos);
         return $this->render('default/show.html.twig', array(
             'base_dir' => realpath($this->container->getParameter('kernel.root_dir').'/..').DIRECTORY_SEPARATOR,'autos'=>$result
         ));
@@ -75,15 +34,5 @@ class DefaultController extends Controller
 
 
 
-    public function getPrice($intraval) {
-
-        $prices=explode("-",$intraval);
-
-        // return the highest price
-         return $prices[1];
-
-
-
-    }
 
 }
--- src/AppBundle/Service/AutoService.php
+++ src/AppBundle/Service/AutoService.php
@@ -7,25 +7,76 @@
  */
 
 
-
-
-
 namespace AppBundle\Service;
 
 
 class AutoService
-{
-    public function getHappyMessage()
-    {
-        $link='https://mitarbeiterautohaus.meinauto.de/data/modelle/17d323d6fd823d9e7f8953085bab63f6';
+{  
+   
+    // this is hard-coded : it should be normally saved as an ENV variable and loaded via the DI and Ijected via the DI. 
+    // I can pass the env variable in the docker-compose file ,and then reload it .....
+    // see what i did to the em variable 
+    $link='https://mitarbeiterautohaus.meinauto.de/data/modelle/17d323d6fd823d9e7f8953085bab63f6';
 
-       $autos=file_get_contents($link);
-
-
-
-
-        return $messages[$index];
+    $protected $entitymanager;
+    // the $em will be injected from the DI into this container
+    public function __construct($em){
+    $this->$entitymanager=$em;
     }
+
+    /**
+     * @var $interval : string 
+     * split a string into two parts , and return the second value : used to return the highest price
+     * ex usage : 77 - 100 as input will return 100
+     */
+    public function getPrice($intraval) {
+        $prices=explode("-",$intraval);
+        // return the highest price
+         return $prices[1];
+    }
+    
+    public function savetoDatabase($autos) {
+        $prices=explode("-",$intraval);
+        // return the highest price
+         return $prices[1];
+    }
+
+    public function fetchApi(){
+        $result =[];
+        // fetch the endpoint
+        $autos=file_get_contents($this->$link);
+        // store data into an array 
+        $data =simplexml_load_string($autos);
+     
+        foreach ($data->data->brand as $brands)
+        {   // get the attributes 
+            $aux = ($brands->attributes());
+            // get the attribute 'name'
+            $name=(string)($aux['name']);
+            // iterate over the modelgroup elements 
+            foreach ($brands->modelgroup as $modelgroup)
+            {   // iterate over the models group
+               foreach ($modelgroup->model as $model){
+                   // get the discount varibale , split it and get the right part of the variable (the highest price)
+                   $highest_price=$this->getPrice($model->discount);
+
+                   // if it doesnt exist or its value is higher than  the already saved value, replace it.
+                   
+                   if (!(array_key_exists($name, $result) ) || ($result[$name] <= $highest_price))
+                       $result[$name]=$highest_price;
+               }
+            }
+        }
+        // return an asso-array where keys are the auto 's names and value are the highest price
+       return $result;
+
+
+    }
+
+
+
+
+
 
 
 }
